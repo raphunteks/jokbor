9904e348221ee4db6056366aecc8984d29d32d86
raphunteks <ksanihtest@gmail.com>
2026-08-30 11:47:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const { speedInsights } = require('@vercel/speed-insights/express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
