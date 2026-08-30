1d9301490cac7e8259a04f631f74a141b368e6e2
raphunteks <ksanihtest@gmail.com>
2026-08-30 15:12:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static(path.join(__dirname, 'public'), {
   }
 }));
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Trust Proxy untuk deployment Vercel / Serverless
 app.enable('trust proxy');
